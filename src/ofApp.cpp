db2279f9c4e707348ba11aff1c5e1a9ff1c5ea94
otoan7010 <naotori7010@gmail.com>
2025-08-07 17:17:03 +0900

    25/8/7

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -6,65 +6,253 @@ ofVec2f preMousePos;
 
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
 	//カメラの設定
 
-		// cam の初期設定（任意）
-		cam.setDistance(300); // 視点と対象の距離
-		cam.setNearClip(0.5);
-		cam.setFarClip(10000);
-		cam.setFov(110); // 視野角（好みで調整）
+	// cam の初期設定（任意）
+	cam.setDistance(300); // 視点と対象の距離
+	cam.setNearClip(0.5);
+	cam.setFarClip(10000);
+	cam.setFov(110); // 視野角（好みで調整）
 
 	ofDisableArbTex(); //GL_TEXTURE_2D モードでテクスチュアを生成
 	ofEnableDepthTest(); //深さ判定を有効に
-	
-	//tex1
-	img1.load("space.jpg"); //画像ファイルは bin の下の data フォルダに入れる
-	tex1.loadData(img1);
-	tex1.setTextureWrap(GL_REPEAT, GL_REPEAT); //縦横とも繰り返し
-	tex1.setTextureMinMagFilter(GL_LINEAR, GL_LINEAR); //補間方法を線形補間に
-	tex1.texData.tex_t = 10; //横の繰り返し数
-	tex1.texData.tex_u = 10; //縦の繰り返し数
-	//tex2
-	img2.load("earth.jpg"); //画像ファイルは bin の下の data フォルダに入れる
-	tex2.loadData(img2);
-}
 
+	//tex1
+	img_space.load("space.jpg"); //画像ファイルは bin の下の data フォルダに入れる
+	tex_space.loadData(img_space);
+	tex_space.setTextureWrap(GL_REPEAT, GL_REPEAT); //縦横とも繰り返し
+	tex_space.setTextureMinMagFilter(GL_LINEAR, GL_LINEAR); //補間方法を線形補間に
+	tex_space.texData.tex_t = 10; //横の繰り返し数
+	tex_space.texData.tex_u = 10; //縦の繰り返し数
+
+	//sun
+	img_sun.load("sun.jpg");
+	tex_sun.loadData(img_sun);
+	//mercury
+	img_mercury.load("mercury.jpg");
+	tex_mercury.loadData(img_mercury);
+	//venus
+	img_venus.load("venus.jpg");
+	tex_venus.loadData(img_venus);
+	//earth
+	img_earth.load("earth.jpg");
+	tex_earth.loadData(img_earth);
+	//moon
+	img_moon.load("moon.jpg");
+	tex_moon.loadData(img_moon);
+	//mars
+	img_mars.load("mars.jpg");
+	tex_mars.loadData(img_mars);
+	//jupiter
+	img_jupiter.load("jupiter.jpg");
+	tex_jupiter.loadData(img_jupiter);
+	//saturn
+	img_saturn.load("saturn.jpg");
+	tex_saturn.loadData(img_saturn);
+	//saturn2
+	img_saturn2.load("saturn2.jpg");
+	tex_saturn2.loadData(img_saturn2);
+	//uranus
+	img_uranus.load("uranus.jpg");
+	tex_uranus.loadData(img_uranus);
+	//uranus2
+	img_uranus2.load("uranus2jpg");
+	tex_uranus2.loadData(img_uranus2);
+	//neptune
+	img_neptune.load("neptune.jpg");
+	tex_neptune.loadData(img_neptune);
+
+	ofBackground(0);
+
+	for (int i = 0; i < numStars; i++) {
+		float x = ofRandom(-5000, 5000);
+		float y = ofRandom(-5000, 5000);
+		float z = ofRandom(-5000, 5000);
+		stars.push_back(ofVec3f(x, y, z));
+	}
+}
 //--------------------------------------------------------------
 void ofApp::update(){
-	
+	// カメラを対象の惑星に移動させる（オフセットあり）
+	ofVec3f targetPos = planetPositions[cameraTarget];
+	ofVec3f camPos = targetPos + ofVec3f(0, 50, 200); // 惑星から少し離れた位置
+	cam.setPosition(camPos);
+	cam.lookAt(targetPos);
+
+
+	a_mercury += 0.16;
+	a_venus += 0.12;
+	a_earth += 0.1;
+	a_moon += 0.08;
+	a_mars += 0.08;
+	a_jupiter += 0.05;
+	a_saturn += 0.04;
+	a_uranus += 0.03;
+	a_neptune += 0.02;
+
+	r_mercury += 0.5;
+	r_venus += -0.1;
+	r_earth += 2;
+	r_moon += 2;
+	r_mars += 1.8;
+	r_jupiter += 4;
+	r_saturn += 3.5;
+	r_uranus += -2;
+	r_neptune += 1.5;
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw() {
 	cam.begin();
-	ofEnableDepthTest(); //深さ判定を有効に
-	ofEnableSmoothing(); //スムージング処理を有効に
-	
-	//テクスチャにも色の設定できる
-	
-	ofPushMatrix();
-	tex1.bind();
-	ofDrawBox(9000);
-	ofRotateDeg(90, 0, 1, 0);
-	tex1.unbind();
-	ofPopMatrix();
-	
+	ofEnableDepthTest();
+	ofEnableSmoothing();
 
-	//tex2 をバインドする
-	tex2.bind();
-	ofDrawSphere(10);
-	tex2.unbind();
-	
+
+	// 星
+	ofSetColor(255); 
+	for (auto & star : stars) {
+		ofDrawSphere(star, 3); 
+	}
+
+	// 太陽
+	ofPushMatrix();
+	planetPositions[0].set(0, 0, 0);
+	tex_sun.bind();
+	ofDrawSphere(0, 0, 0, 100); 
+	tex_sun.unbind();
+	ofPopMatrix();
+
+	// 水星
+	ofPushMatrix();
+	float mercuryDist = 150;
+	float mercuryX = cos(ofDegToRad(a_mercury)) * mercuryDist;
+	float mercuryZ = sin(ofDegToRad(a_mercury)) * mercuryDist;
+	planetPositions[1].set(mercuryX, 0, mercuryZ);
+	ofTranslate(mercuryX, 0, mercuryZ);
+	ofRotateDeg(r_mercury, 0, 1, 0); 
+	tex_mercury.bind();
+	ofDrawSphere(4); 
+	tex_mercury.unbind();
+	ofPopMatrix();
+
+	// 金星
+	ofPushMatrix();
+	float venusDist = 220;
+	float venusX = cos(ofDegToRad(a_venus)) * venusDist;
+	float venusZ = sin(ofDegToRad(a_venus)) * venusDist;
+	planetPositions[2].set(venusX, 0, venusZ);
+	ofTranslate(venusX, 0, venusZ);
+	ofRotateDeg(r_venus, 0, 1, 0); 
+	tex_venus.bind();
+	ofDrawSphere(9);
+	tex_venus.unbind();
+	ofPopMatrix();
+
+	// 地球
+	ofPushMatrix();
+	float earthDist = 300; 
+	float earthX = cos(ofDegToRad(a_earth)) * earthDist;
+	float earthZ = sin(ofDegToRad(a_earth)) * earthDist;
+	planetPositions[3].set(earthX, 0, earthZ);
+	ofTranslate(earthX, 0, earthZ); 
+	ofRotateDeg(r_earth, 0, 1, 0); 
+	tex_earth.bind();
+	ofDrawSphere(0, 0, 0, 10); 
+	tex_earth.unbind();
+	ofPopMatrix();
+
+	// 月（地球の周りに配置）
+	ofPushMatrix();
+	ofTranslate(earthX, 0, earthZ);
+	float moonDist = 30;
+	float moonX = cos(ofDegToRad(a_moon)) * moonDist;
+	float moonZ = sin(ofDegToRad(a_moon)) * moonDist;
+	ofTranslate(moonX, 0, moonZ);
+	ofRotateDeg(r_moon, 0, 1, 0);
+	tex_moon.bind();
+	ofDrawSphere(3);
+	tex_moon.unbind();
+	ofPopMatrix();
+
+	// 火星
+	ofPushMatrix();
+	float marsDist = 400;
+	float marsX = cos(ofDegToRad(a_mars)) * marsDist;
+	float marsZ = sin(ofDegToRad(a_mars)) * marsDist;
+	planetPositions[4].set(marsX, 0, marsZ);
+	ofTranslate(marsX, 0, marsZ);
+	ofRotateDeg(r_mars, 0, 1, 0);
+	tex_mars.bind();
+	ofDrawSphere(5.3);
+	tex_mars.unbind();
+	ofPopMatrix();
+
+	// 木星
+	ofPushMatrix();
+	float jupiterDist = 550;
+	float jupiterX = cos(ofDegToRad(a_jupiter)) * jupiterDist;
+	float jupiterZ = sin(ofDegToRad(a_jupiter)) * jupiterDist;
+	planetPositions[5].set(jupiterX, 0, jupiterZ);
+	ofTranslate(jupiterX, 0, jupiterZ);
+	ofRotateDeg(r_jupiter, 0, 1, 0);
+	tex_jupiter.bind();
+	ofDrawSphere(30);
+	tex_jupiter.unbind();
+	ofPopMatrix();
+
+	// 土星
+	ofPushMatrix();
+	float saturnDist = 700;
+	float saturnX = cos(ofDegToRad(a_saturn)) * saturnDist;
+	float saturnZ = sin(ofDegToRad(a_saturn)) * saturnDist;
+	planetPositions[6].set(saturnX, 0, saturnZ);
+	ofTranslate(saturnX, 0, saturnZ);
+	ofRotateDeg(r_saturn, 0, 1, 0);
+	tex_saturn.bind();
+	ofDrawSphere(25);
+	tex_saturn.unbind();
+	ofPopMatrix();
+
+	// 天王星
+	ofPushMatrix();
+	float uranusDist = 850;
+	float uranusX = cos(ofDegToRad(a_uranus)) * uranusDist;
+	float uranusZ = sin(ofDegToRad(a_uranus)) * uranusDist;
+	planetPositions[7].set(uranusX, 0, uranusZ);
+	ofTranslate(uranusX, 0, uranusZ);
+	ofRotateDeg(r_uranus, 0, 1, 0);
+	tex_uranus.bind();
+	ofDrawSphere(15);
+	tex_uranus.unbind();
+	ofPopMatrix();
+
+	// 海王星
+	ofPushMatrix();
+	float neptuneDist = 1000;
+	float neptuneX = cos(ofDegToRad(a_neptune)) * neptuneDist;
+	float neptuneZ = sin(ofDegToRad(a_neptune)) * neptuneDist;
+	planetPositions[8].set(neptuneX, 0, neptuneZ);
+	ofTranslate(neptuneX, 0, neptuneZ);
+	ofRotateDeg(r_neptune, 0, 1, 0);
+	tex_neptune.bind();
+	ofDrawSphere(14);
+	tex_neptune.unbind();
+	ofPopMatrix();
+
 	cam.end();
 }
 
 
 
-//--------------------------------------------------------------
-void ofApp::keyPressed(int key){
 
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+	if (key >= '0' && key <= '8') {
+		cameraTarget = key - '0';
+	}
 }
+
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
--- src/ofApp.h
+++ src/ofApp.h
@@ -24,9 +24,29 @@ class ofApp : public ofBaseApp{
 		ofEasyCam cam; //ofCamera クラスのインスタンス cam を生成
 		ofLight light; //ofLight クラスのインスタンス light0 を作成
 		ofMaterial material[3]; //ofMateial クラスのインスタンスを作成
-		ofVec3f r;
+		vector<ofVec3f> stars;
+		int numStars = 1000;
 		ofVec3f preMousePos;
-		ofImage img1, img2, img3;
-		ofTexture tex1, tex2, tex3;
-		
+		ofImage img_space, img_sun, img_mercury,
+			    img_venus, img_earth,  img_moon, img_mars,
+			    img_jupiter, img_saturn, img_saturn2,
+			    img_uranus, img_uranus2, img_neptune;
+
+		ofTexture tex_space, tex_sun, tex_mercury,
+			      tex_venus, tex_earth, tex_moon, tex_mars,
+			      tex_jupiter, tex_saturn, tex_saturn2,
+			      tex_uranus, tex_uranus2, tex_neptune;
+
+		float r_space, r_sun, r_mercury,
+			    r_venus, r_earth, r_moon, r_mars,
+			    r_jupiter, r_saturn, r_saturn2,
+			    r_uranus, r_uranus2, r_neptune;
+
+		float a_space, a_sun, a_mercury,
+			a_venus, a_earth, a_moon, a_mars,
+			a_jupiter, a_saturn, a_saturn2,
+			a_uranus, a_uranus2, a_neptune;
+
+		int cameraTarget = 0; // 0: 太陽, 1: 水星, ..., 8: 海王星
+		ofVec3f planetPositions[9];
 };
